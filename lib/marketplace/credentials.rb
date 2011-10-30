13a0abf5e52f1cabb01848dfe573708658ce88e5
Dave Ott <daveott@daveott.net>
2011-10-30 11:19:10 -0400

    Options keys are strings

--- lib/marketplace/credentials.rb
+++ lib/marketplace/credentials.rb
@@ -7,11 +7,11 @@ module Marketplace
 
     attr_accessor :merchant, :marketplace, :aws_access_key_id, :secret_key
 
-    def initialize(options={})
-      self.merchant          = options[:merchant]
-      self.marketplace       = options[:marketplace]
-      self.aws_access_key_id = options[:aws_access_key_id]
-      self.secret_key        = options[:secret_key]
+    def initialize(options)
+      self.merchant          = options["merchant"]
+      self.marketplace       = options["marketplace"]
+      self.aws_access_key_id = options["aws_access_key_id"]
+      self.secret_key        = options["secret_key"]
     end
     
     def self.instance
@@ -19,7 +19,15 @@ module Marketplace
     end
 
     def self.read_from_file
-      YAML.load_file(File.join('config', 'credentials.yml'))
+      YAML.load_file(File.join(root_directory, 'config', 'credentials.yml'))
+    end
+
+    def self.current_directory
+      File.dirname(__FILE__)
+    end
+
+    def self.root_directory
+      current_directory + "/../.."
     end
   end
 end
