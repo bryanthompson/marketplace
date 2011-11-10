9c9785adf765254786d6c627ca10d68a1d133343
Dave Ott <daveott@daveott.net>
2011-11-10 16:50:54 -0500

    WIP

--- lib/marketplace/query_string.rb
+++ lib/marketplace/query_string.rb
@@ -3,37 +3,36 @@ module Marketplace
   require 'active_support/core_ext'
 
   class QueryString
-    attr_writer :parts
-    attr_accessor :path
+    attr_accessor :path, :parameters, :sorted_parameters
 
-    def initialize(path, parts)
-      raise Marketplace::Exceptions::QueryStringArgumentError unless parts
-      self.parts = parts
+    def initialize(path, parameters)
+      self.parameters = parameters
       self.path = path
+      raise Marketplace::Exceptions::QueryStringArgumentError unless parameters.present?
     end
 
-    def to_canonical
-      CGI.escape(parameters)
-    end
-
-    def parts=(parts)
-      @parts ||= parts
+    def parameters=(value)
+      @parameters ||= value
         .merge(timestamp.to_params)
         .merge(credentials.to_params)
         .merge(Marketplace::Signature.to_params)
         .merge(AWSAccessKeyId: credentials.aws_access_key_id)
     end
 
-    def parts
-      Hash[@parts.map { |k,v| [camelize(k),v] }.sort]
+    def sorted_parameters
+      Hash[parameters.map { |k,v| [camelize(k),v] }.sort]
     end
 
     def parameters
-      "?" + parts.to_query
+      "?" + @parameters.to_query
+    end
+
+    def to_canonical
+      CGI.escape(parameters)
     end
 
     def to_hash
-      parts.merge("Signature" => signature.sign!)
+      sorted_parameters.merge("Signature" => signature.sign!)
     end
 
     private
--- spec/unit/marketplace/query_string_spec.rb
+++ spec/unit/marketplace/query_string_spec.rb
@@ -9,12 +9,12 @@ describe Marketplace::QueryString do
     subject { Marketplace::QueryString }
 
     context "with no arguments" do
-      let(:parts) { nil }
+      let(:parameters) { {} }
       let(:message) { "There were no parameters from which to build a query string" }
 
       it "raises an error" do
         expect do
-          subject.new(path, parts)
+          subject.new(path, parameters)
         end.to raise_error(Marketplace::Exceptions::QueryStringArgumentError, message)
       end
     end
@@ -24,16 +24,16 @@ describe Marketplace::QueryString do
     let(:params) { { A: 1 } }
     let(:query_string) { Marketplace::QueryString.new(path, params) }
     subject { query_string.to_canonical }
-    before { query_string.stub(:parts).and_return(params) }
+    before { query_string.stub(:parameters).and_return(params) }
 
     it "escapes the parameters" do
       subject.should == "%3FA%3D1"
     end
   end
 
-  describe "#parts" do
+  describe "#parameters" do
     let(:query_string) { Marketplace::QueryString.new(path, {foo: "bar"}) }
-    subject { query_string.parts }
+    subject { query_string.parameters }
 
     it "titleizes all keys in the hash" do
       subject.should have_key("Foo")
@@ -41,16 +41,16 @@ describe Marketplace::QueryString do
   end
 
   describe "#parameters" do
-    subject { Marketplace::QueryString.new(path, parts) }
-    before { subject.stub(:parts).and_return(parts) }
+    subject { Marketplace::QueryString.new(path, parameters) }
+    before { subject.stub(:parameters).and_return(parameters) }
 
     context "with 1 parameter" do
-      let(:parts) { { A: 1 } }
+      let(:parameters) { { A: 1 } }
       its(:parameters) { should == "?A=1" }
     end
 
     context "with 2 parameters" do
-      let(:parts) { { "A" => 1, "B" => 2 } }
+      let(:parameters) { { "A" => 1, "B" => 2 } }
       its(:parameters) { should == "?A=1&B=2" }
     end
   end
