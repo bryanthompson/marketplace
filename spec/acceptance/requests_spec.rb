aa8e0d4a2a1a2ad240fa34a20ea51f778e6a7555
Dave Ott <daveott@daveott.net>
2012-03-30 14:35:06 -0400

    Remove unused spec

--- spec/acceptance/requests_spec.rb
+++ spec/acceptance/requests_spec.rb
@@ -6,12 +6,4 @@ describe "Requests" do
     let(:method) { :get }
     it_behaves_like "a request"
   end
-
-  describe "posts" do
-    let(:url) { "http://example.com/" }
-    let(:method) { :post }
-    it "posts" do
-      pending
-    end
-  end
 end
